cd17f8ffe737da81ca7f81bbd773e3792b68ebd2
Taras Budko <taras1990wot@gmail.com>
2022-07-27 13:35:24 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+let obj = {
+    name: 'Taras',
+    age: '20',
+    isStudent: true
+};
+console.log(obj.isStudent);
+
+let arr =['Taras', 20, true];
+console.log(arr[0]);
+
+alert ('Hello');
+
+const result = confirm('Are you here?');
+console.log(result);
+
+const answer = prompt('Вам є 18?', '18');
+console.log(answer);
