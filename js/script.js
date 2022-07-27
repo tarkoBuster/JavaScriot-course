d102f3e03ab5dd9868e3a7d9b2442c1ab3f8ebb2
Taras Budko <taras1990wot@gmail.com>
2022-07-27 16:08:55 +0300

    first lesson film

--- js/script.js
+++ js/script.js
@@ -1,25 +1,45 @@
 "use strict";
 
-let number = 5;
-const leftBorderWidth = 1;
+// let number = 5;
+// const leftBorderWidth = 1;
 
-number = 10;
-console.log(number);
+// number = 10;
+// console.log(number);
 
-let obj = {
-    name: 'Taras',
-    age: '20',
-    isStudent: true
+// let obj = {
+//     name: 'Taras',
+//     age: '20',
+//     isStudent: true
+// };
+// console.log(obj.isStudent);
+
+// let arr =['Taras', 20, true];
+// console.log(arr[0]);
+
+// alert ('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt('Вам є 18?', '18');
+// console.log(answer);
+
+const numberOfFilms = +prompt('How many films you watch?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
-console.log(obj.isStudent);
 
-let arr =['Taras', 20, true];
-console.log(arr[0]);
+const a = prompt('Один з останніх фільмів?', ''),
+      b = prompt('Оцінка йому?', ''),
+      c = prompt('Один з останніх фільмів?', ''),
+      d= prompt('Оцінка йому?', '');
 
-alert ('Hello');
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-const result = confirm('Are you here?');
-console.log(result);
-
-const answer = prompt('Вам є 18?', '18');
-console.log(answer);
+console.log(personalMovieDB);
