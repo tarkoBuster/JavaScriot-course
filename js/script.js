32df25809b7121ad70d748bd2091d1907a61ed63
tarkoBuster <taras1990WOT@gmail.com>
2022-08-10 15:56:43 +0300

    massive + quickSort

--- js/script.js
+++ js/script.js
@@ -23,72 +23,73 @@
 // const answer = prompt('Вам є 18?', '18');
 // console.log(answer);
 
-let numberOfFilms;
+//Практика
+// let numberOfFilms;
 
-function start() {
-  numberOfFilms = +prompt("How many films you watch?", "");
+// function start() {
+//   numberOfFilms = +prompt("How many films you watch?", "");
 
-  while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-    numberOfFilms = +prompt("How many films you watch?", "");
-  }
-}
-start();
+//   while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+//     numberOfFilms = +prompt("How many films you watch?", "");
+//   }
+// }
+// start();
 
-const personalMovieDB = {
-  count: numberOfFilms,
-  movies: {},
-  actors: {},
-  genres: [],
-  privat: false,
-};
+// const personalMovieDB = {
+//   count: numberOfFilms,
+//   movies: {},
+//   actors: {},
+//   genres: [],
+//   privat: false,
+// };
 
-function rememberMyFilms() {
-  for (let i = 0; i < 2; i++) {
-    const a = prompt("Один з останніх фільмів?", ""),
-      b = prompt("Оцінка йому?", "");
+// function rememberMyFilms() {
+//   for (let i = 0; i < 2; i++) {
+//     const a = prompt("Один з останніх фільмів?", ""),
+//       b = prompt("Оцінка йому?", "");
 
-    if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-      personalMovieDB.movies[a] = b;
-      console.log("done");
-    } else {
-      i--;
-    }
-  }
-}
-rememberMyFilms();
+//     if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+//       personalMovieDB.movies[a] = b;
+//       console.log("done");
+//     } else {
+//       i--;
+//     }
+//   }
+// }
+// rememberMyFilms();
 
-function detectPersonalLevel() {
-  if (personalMovieDB.count < 10) {
-    console.log("beginner");
-  } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("meddle");
-  } else if (personalMovieDB.count >= 30) {
-    console.log("senior");
-  } else {
-    console.log("error");
-  }
-}
-detectPersonalLevel();
+// function detectPersonalLevel() {
+//   if (personalMovieDB.count < 10) {
+//     console.log("beginner");
+//   } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log("meddle");
+//   } else if (personalMovieDB.count >= 30) {
+//     console.log("senior");
+//   } else {
+//     console.log("error");
+//   }
+// }
+// detectPersonalLevel();
 
-function showMyDB(hidden) {
-  if (!hidden) {
-    console.log(personalMovieDB);
-  }
-}
+// function showMyDB(hidden) {
+//   if (!hidden) {
+//     console.log(personalMovieDB);
+//   }
+// }
 
-showMyDB(personalMovieDB.privat);
+// showMyDB(personalMovieDB.privat);
 
-function writeYourGenres() {
-  for (let i = 1; i <= 3; i++) {
-    personalMovieDB.genres[i - 1] = prompt(
-      `Ваш улюблений жанр під номером ${i}`
-    );
-  }
-}
+// function writeYourGenres() {
+//   for (let i = 1; i <= 3; i++) {
+//     personalMovieDB.genres[i - 1] = prompt(
+//       `Ваш улюблений жанр під номером ${i}`
+//     );
+//   }
+// }
 
-writeYourGenres();
+// writeYourGenres();
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
 
 //оператор
 // if (4 == 4) {
@@ -115,3 +116,29 @@ console.log(personalMovieDB);
 
 //     console.log(i);
 // }
+
+//Масиви та псевдомасиви
+const arr = [1, 2, 3, 6, 7, 8];
+
+// arr.forEach(function(item, i, arr) {
+//   console.log(`${i}: ${item} в середині масиву ${arr}`);
+// })
+
+// arr.pop();
+// arr.push(10);
+
+// console.log(arr);
+
+// for(let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// for (let value of arr) {
+//   console.log(value);
+// }
+
+const str = prompt('', '');
+const products = str.split(', ');
+// console.log(products);
+products.sort();
+console.log(products.join('; '));
